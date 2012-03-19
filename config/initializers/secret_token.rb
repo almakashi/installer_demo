2576287c51146f1563eb1a4cf42fe7dd5ee69655
Nacer Abdoulaye Garga <nacerious@yahoo.fr>
2012-03-18 23:42:51 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InsatallerDemo::Application.config.secret_token = '717fa12d3e6f4df9a5b8f53c2ea3af4198794b2f960d9b1acfff317070cc7841b410967495e08c0e881912aff82cef06bb3d7481c3f6b9be0fbfa587e25c99a5'
